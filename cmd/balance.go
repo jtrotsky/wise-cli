8bf48ff054e481c0b09cbf4b9e4e17038ae58ad4
Joseph Armstrong <5625795+jtrotsky@users.noreply.github.com>
2021-02-24 16:53:36 +0000

    multiple bals

--- cmd/balance.go
+++ cmd/balance.go
@@ -13,6 +13,7 @@ type balanceCmd struct {
 
 	client    client.Client
 	profileID int64
+	currency  string
 }
 
 func newBalanceCmd() *balanceCmd {
@@ -29,7 +30,8 @@ $ wise balance convert --amount 100 --from GBP --to NZD`,
 		RunE: bc.runBalanceCmd,
 	}
 
-	bc.cmd.PersistentFlags().StringVar(&bc.client.APIKey, "token", "", "API token")
+	bc.cmd.PersistentFlags().StringVar(&bc.client.APIToken, "token", "", "API token")
+	bc.cmd.Flags().StringVar(&bc.currency, "currency", "GBP", "The account currency (e.g. GBP)")
 
 	return bc
 }
